8da6c40bcc7e9fb6d7c1de85682591d8362e31f9
marci <marci@DESKTOP-V0DL4NJ>
2024-06-02 17:33:33 +0200

    proste wyświetlanie obrazków przy produktach na stronie głównej

--- PZ_Projekt/Data/Migrations/20240602145822_Image1.Designer.cs
+++ PZ_Projekt/Data/Migrations/20240602145822_Image1.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PZ_Projekt.Data;
 
@@ -11,9 +12,11 @@ using PZ_Projekt.Data;
 namespace PZ_Projekt.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240602145822_Image1")]
+    partial class Image1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -237,6 +240,7 @@ namespace PZ_Projekt.Data.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("ImageUrl")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Name")
